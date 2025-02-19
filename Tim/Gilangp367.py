0c358ae250063f7680149876349b5bc3ee40b7e2
SiwoMikza <169017319+SiwoMikza@users.noreply.github.com>
2025-02-19 19:55:30 +0700

    Add files via upload

---
+++ Tim/Gilangp367.py
@@ -0,0 +1,9 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM  : ", nim)
+    print("Peran saya yaitu : ", peran)
+if __name__ == "__main__":
+    nama = "Gilang Pamungkas"
+    nim = "2213010367"
+    peran = "Ketua : Desain Tampilan Aplikasi"
+    perkenalan(nama, nim, peran)
