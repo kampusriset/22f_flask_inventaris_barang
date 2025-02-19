81cde6aa0af4ca32d4f5dc36f58dea78b2972584
rizky-anggara <162684816+rizky-anggara@users.noreply.github.com>
2025-02-19 19:22:55 +0700

    Add files via upload

---
+++ Tim/MuhRizkyAnggaraYunanPutra.py
@@ -0,0 +1,11 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM  : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+# Pastikan bagian ini tidak berada di dalam fungsi perkenalan
+if __name__ == "__main__":
+    nama = "Muh Rizky Anggara Yunan Putra"
+    nim = "2213010372"
+    peran = "Desainer: Desain Tampilan Aplikasi"
+    perkenalan(nama, nim, peran)
